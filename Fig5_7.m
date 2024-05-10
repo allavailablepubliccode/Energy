e0364cf824cc99ea64db45fbe47382e6262bc02d
allavailablepubliccode <45461816+allavailablepubliccode@users.noreply.github.com>
2024-05-10 14:45:25 +0200

    Fig5_7.m

--- Fig5_7.m
+++ Fig5_7.m
@@ -1,17 +1,12 @@
 clear;clc;close all;
 addpath(genpath('~/Dropbox/BrainSpace/matlab/'))
-load('/Users/erik/Library/CloudStorage/Dropbox/Least_Action/null/10000_cmruglu.mat','gldata')
-gldatafull = [gldata{1,1};gldata{1,2}];
+dir   = '/Users/erik/Library/CloudStorage/Dropbox/Work/Least_Action/';
+load([dir 'Hmat.mat'],'H')
+[surf_lh, surf_rh] = load_conte69();
 labeling           = load_parcellation('schaefer',100);
 
-% ask fez
+load('/Users/erik/Library/CloudStorage/Dropbox/Work/Least_Action/null/10000_cmruglu.mat','gldata')
+gldatafull = [gldata{1,1};gldata{1,2}];
 fez_data            = full2parcel(gldatafull',labeling.schaefer_100);
 
-[surf_lh, surf_rh] = load_conte69();
-plot_hemispheres(fez_data, {surf_lh,surf_rh},'parcellation',labeling.schaefer_100,'views','ms');
-colormap gray
-view(-90,0)
-% colorbar
-
-
-
+plot(fez_data,mean(H)','o')
